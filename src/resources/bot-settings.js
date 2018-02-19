76aca8b35daa3c8c12278e87df4305df5e9bb9b0
storycraft <dbsxoal123@naver.com>
2018-02-19 11:15:38 +0900

    init

---
+++ src/resources/bot-settings.js
@@ -0,0 +1,29 @@
+export default {
+    //디스코드 봇 설정
+    "discord": {
+        "enabled": true,
+
+        "clientId": "412451088252272642",
+        "clientSecret": "B3am4vMVkbANR5dNa2Y6uYVIdaIuGOdz",
+
+        "userToken": "NDEyNDUxMDg4MjUyMjcyNjQy.DWKeTQ.aBS0L4a4gH5MRMP7OSYeF9aCuwk"
+    },
+
+    //라인 챗 봇 설정
+    "line": {
+        "enabled": true,
+
+        "channelId": "",
+        "channelSecret": "",
+
+        "channelAccessToken": 0
+    },
+
+    //페이스북 메신져 설정
+    "facebook": {
+        "enabled": true,
+
+        "email": "",
+        "password": "",
+    }
+}
