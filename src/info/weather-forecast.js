3b882ee81ef9a4eb15552a56a5bc0f197cf752b6
storycraft <dbsxoal123@naver.com>
2018-02-23 02:26:51 +0900

    added 'Powered by Dark Sky' text https://darksky.net/dev/docs/terms#attribution

--- src/info/weather-forecast.js
+++ src/info/weather-forecast.js
@@ -96,7 +96,9 @@ ${currentWeather['summary']}
 풍속: ${currentWeather['windSpeed']} m/s ,가시 거리: ${currentWeather['visibility']} km`;
 
                             if (currentWeather['precipType'])
-                                infoText += `\n${PRECIP_DESCRIPTION[currentWeather['precipType']]} 이(가) 내릴 확률 ${currentWeather['precipProbability'] * 100} %`;
+                                infoText += `\n${PRECIP_DESCRIPTION[currentWeather['precipType']]} 이(가) 내릴 확률 ${currentWeather['precipProbability'] * 100} %\n`;
+                            
+                            infoText += '`Powered by Dark Sky`'
 
                             this.editStatus(msg, infoText);
 
