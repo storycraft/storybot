313e11c2029a5c66c6cc038c54a6aafe1bc9e75b
storycraft <dbsxoal123@naver.com>
2018-03-30 21:04:53 +0900

    fix test command

--- dist/math/calculator-command.js
+++ dist/math/calculator-command.js
@@ -8,13 +8,14 @@ var _storybotCore = require('storybot-core');
 
 class CalculatorCommand extends _storybotCore.CommandListener {
     constructor(main) {
+        super();
         this.main = main;
 
         this.main.CommandManager.on('calc', this.onCommand.bind(this));
     }
 
     get Description() {
-        return '계산기 쓰실 | *calc <계산식>';
+        return '계산기 쓰실 **미완성 기능** | *calc <계산식>';
     }
 
     get Aliases() {
--- src/math/calculator-command.js
+++ src/math/calculator-command.js
@@ -2,13 +2,14 @@ import { CommandListener } from "storybot-core";
 
 export default class CalculatorCommand extends CommandListener {
     constructor(main){
+        super();
         this.main = main;
 
         this.main.CommandManager.on('calc', this.onCommand.bind(this));
     }
 
     get Description(){
-        return '계산기 쓰실 | *calc <계산식>';
+        return '계산기 쓰실 **미완성 기능** | *calc <계산식>';
     }
 
     get Aliases(){
