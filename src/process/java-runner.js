b2dd373b4103a785b1763b8b5298f33ec5401e29
storycraft <dbsxoal123@naver.com>
2018-04-01 21:32:06 +0900

    restore from april fool

--- src/process/java-runner.js
+++ src/process/java-runner.js
@@ -127,7 +127,7 @@ export default class JavaRunner extends ProgramRunner {
 
         if (this.first){
             this.first = false;
-            channel.send('해당 소스 실행으로 인한 채널 메세지 피해는 Dotorybot에서 책임 지지 않습니다');
+            channel.send('해당 소스 실행으로 인한 채널 메세지 피해는 Storybot에서 책임 지지 않습니다');
         }
 
         this.run(source, mainClass, channel);
