35a4f23d47961cbbac587d05b6d7b2ed1ccd0107
storycraft <dbsxoal123@naver.com>
2019-02-13 16:15:08 +0900

    reenable program runner with nobody uid

--- src/process/process.js
+++ src/process/process.js
@@ -52,7 +52,7 @@ export default class Process extends EventEmitter {
     }
 
     createProcess(workDir, args){
-        return childProcess.spawn(this.StartCommand, args, {cwd: workDir,encoding: 'utf8', detached: true});
+        return childProcess.spawn(this.StartCommand, args, {cwd: workDir,encoding: 'utf8', detached: true, 'uid': 65534, 'gid': 65534});
     }
 
     start(workDir, ...args){
